9f547b0c0bf59244a6529637283dc2c62377ed80
jcaiaffa <jonathan.caiaffa@akendos.com>
2017-01-18 14:55:54 -0500

    white bg class

--- wp-content/themes/Jason Debus Heigl Foundation/single.php
+++ wp-content/themes/Jason Debus Heigl Foundation/single.php
@@ -6,22 +6,22 @@
  */
 ?>
 <?php get_header(); ?>
-<div class="container">
-	<div class="row">
-		<div class="col-lg-12">
-			<div>
-				<?php
-				if (have_posts()) :
-					while (have_posts()) :
-						the_post();
-						the_author_posts_link();
-						the_content();
-					endwhile;
-				endif;
-				?>
+<div class="white-bg">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div>
+					<?php
+					if (have_posts()) :
+						while (have_posts()) :
+							the_post();
+							the_content();
+						endwhile;
+					endif;
+					?>
+				</div>
 			</div>
 		</div>
-	</div>
+	</div>	
 </div>
-
 <?php get_footer(); ?>
