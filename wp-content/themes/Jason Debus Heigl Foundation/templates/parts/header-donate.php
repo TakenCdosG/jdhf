c85bc4789f42ae20b2cc9f58e68f77542e2899c5
Esteban Rocha <estebanrocha.j@gmail.com>
2017-01-19 16:21:27 -0500

    Fix donate page form
    Master * Version_0.1.2
    
    Fixed styles on donation formulary

--- wp-content/themes/Jason Debus Heigl Foundation/templates/parts/header-donate.php
+++ wp-content/themes/Jason Debus Heigl Foundation/templates/parts/header-donate.php
@@ -11,10 +11,11 @@ if( get_sub_field('global') == 1):
 		if( get_row_layout() == 'header' ):
 			?>
 				<div class="header" style="background:url(<?php the_sub_field('background_image') ?>)">
-					<div class="container">
-						<div class="content col-lg-12">
-							<h1 class="heading"><?php the_sub_field('heading') ?></h1>
-							<div class="button"><?php echo do_shortcode(the_sub_field('button')) ?></div>
+					<div class="row">
+						<div class="col-md-12">
+							<div class="text">
+								<?php echo do_shortcode(get_sub_field('heading')) ?>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -23,30 +24,14 @@ if( get_sub_field('global') == 1):
 	endwhile;
 else :
 ?>
-<div class="header" style="background:url(<?php the_sub_field('background_image') ?>)">
-	<!-- <div class="donation-w-bg">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<div class="text">
-						<?php //echo do_shortcode(get_sub_field('heading')) ?>
-					</div>
+<div class="header" style="background:url(<?php echo the_sub_field('background_image') ?>)">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="donation-basic">
+				<div class="text">
+					<?php echo do_shortcode(get_sub_field('heading')) ?>
 				</div>
 			</div>
-		</div>
-	</div> -->
-
-	<div class="container">
-		<div class="content">
-			<h1 class="heading"><?php do_shortcode(get_sub_field('heading'));?></h1>
-			<div class="heading-donate-bar">
-				<p>HELP SAVE THE ANIMALS</p>
-			</div>
-			<div class="heading-donate">
-				<?php echo do_shortcode('[give_form id="47"]'); ?>
-			</div>
-
-			<div class="button"><?php echo do_shortcode(the_sub_field('button')) ?></div>
 		</div>
 	</div>
 </div>
