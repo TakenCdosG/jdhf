00dd195ed8252ba24fcc32606100934bc75471ed
jcaiaffa <jonathan.caiaffa@akendos.com>
2017-01-19 12:00:41 -0500

    Panels fixes

--- wp-content/themes/Jason Debus Heigl Foundation/templates/parts/load_news.php
+++ wp-content/themes/Jason Debus Heigl Foundation/templates/parts/load_news.php
@@ -1,5 +1,6 @@
-<?php if(get_sub_field('load_news') == 1) :  
-	if( get_sub_field('global') == 1):
+<?php 
+if( get_sub_field('global') == 1):
+	if(get_sub_field('load_news') == 1) : 
 	    while ( have_rows('page_content', get_id_by_slug('global-modules')) ) : the_row();
 	        if( get_row_layout() == 'news_listing' ): 
 	        	?>
@@ -50,54 +51,56 @@
 					</div>
 	        	<?php
 	        endif;
-	    endwhile;	        				
-	else :
-	?>
-	<div class="news-listing" style="background:url(<?php the_sub_field('background_image') ?>);">
-		<div class="container">
-			<div class="row">
-				<div class="content col-lg-12">
-					<div class="content">
-						<?php 
-							$args = Array(
-							  'post_type' => 'post',
-							  'posts_per_page' => '4'
-							);
-							// The Query
-							$the_query = new WP_Query( $args );
+	    endwhile;
+	endif;	        				
+else :
+	if(get_sub_field('load_news') == 1) : 
+?>
+<div class="news-listing" style="background:url(<?php the_sub_field('background_image') ?>);">
+	<div class="container">
+		<div class="row">
+			<div class="content col-lg-12">
+				<div class="content">
+					<?php 
+						$args = Array(
+						  'post_type' => 'post',
+						  'posts_per_page' => '4'
+						);
+						// The Query
+						$the_query = new WP_Query( $args );
 
-							// The Loop
-							if ( $the_query->have_posts() ) {
-								while ( $the_query->have_posts() ) {
-									$the_query->the_post();
-								?>
-									<div class="col-md-3"> 
-										<div class="post">
-											<div class="date">
-												<?php echo get_the_date() ?> 
-											</div>
-											<div class="title">
-												<?php echo get_the_title() ?> 
-											</div>
-											<div class="readmore">
-												<a href="<?php echo get_the_permalink() ?> ">Read More</a>
-											</div>											
+						// The Loop
+						if ( $the_query->have_posts() ) {
+							while ( $the_query->have_posts() ) {
+								$the_query->the_post();
+							?>
+								<div class="col-md-3"> 
+									<div class="post">
+										<div class="date">
+											<?php echo get_the_date() ?> 
 										</div>
-										
+										<div class="title">
+											<?php echo get_the_title() ?> 
+										</div>
+										<div class="readmore">
+											<a href="<?php echo get_the_permalink() ?> ">Read More</a>
+										</div>											
 									</div>
-								<?php
-								}
-								/* Restore original Post Data */
-								wp_reset_postdata();
-							} else {
-								// no posts found
+									
+								</div>
+							<?php
 							}
-						?>
-					</div>
-				</div>							
-			</div>			
-		</div>
+							/* Restore original Post Data */
+							wp_reset_postdata();
+						} else {
+							// no posts found
+						}
+					?>
+				</div>
+			</div>							
+		</div>			
 	</div>
-	<?php
+</div>
+<?php
 	endif;
 endif;
