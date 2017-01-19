d2094414a67aff1623a0d6b0068d6be49afd4862
jcaiaffa <jonathan.caiaffa@akendos.com>
2017-01-19 15:26:05 -0500

    Header shortcode fix

--- wp-content/themes/Jason Debus Heigl Foundation/templates/parts/header.php
+++ wp-content/themes/Jason Debus Heigl Foundation/templates/parts/header.php
@@ -7,7 +7,7 @@ if( get_sub_field('global') == 1):
 					<div class="container">
 						<div class="content">	
 							<h1 class="heading"><?php the_sub_field('heading') ?></h1>
-							<div class="button"><?php echo do_shortcode(the_sub_field('button')) ?></div>
+							<div class="button"><?php echo do_shortcode(get_sub_field('button')) ?></div>
 						</div>
 					</div>
 				</div>
