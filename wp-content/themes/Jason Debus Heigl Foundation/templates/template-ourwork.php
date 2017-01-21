71649c1dad02a4b93e7acb5a3b78bcfb2c72c1d8
Esteban Rocha <estebanrocha.j@gmail.com>
2017-01-20 21:38:27 -0500

    Fix css no header
    Master * Version_0.1.6
    
    Fix no headr js not working on live

--- wp-content/themes/Jason Debus Heigl Foundation/templates/template-ourwork.php
+++ wp-content/themes/Jason Debus Heigl Foundation/templates/template-ourwork.php
@@ -10,36 +10,18 @@
 	<?php
 	// check if the flexible content field has rows of data
 	if( have_rows('page_content') ):
-	     // loop through the rows of data
-	    while ( have_rows('page_content') ) : the_row();
-	        if( get_row_layout() == 'header' ):
+		 // loop through the rows of data
+		while ( have_rows('page_content') ) : the_row();
+			if( get_row_layout() == 'header' ):
 				get_template_part( 'templates/parts/header-ourwork' );
-	        elseif( get_row_layout() == 'bio_column_right' ):
+			elseif( get_row_layout() == 'bio_column_right' ):
 				get_template_part( 'templates/parts/bio-column-right' );
-	        elseif( get_row_layout() == 'bio_column_left' ):
+			elseif( get_row_layout() == 'bio_column_left' ):
 				get_template_part( 'templates/parts/bio-column-left' );
-	        elseif( get_row_layout() == 'statistics' ):
-	        	get_template_part( 'templates/parts/statistics' );
-	        elseif( get_row_layout() == 'general_quote' ):
-	        	get_template_part( 'templates/parts/general_quote' );
-	        elseif( get_row_layout() == 'programs' ):
-	        	get_template_part( 'templates/parts/programs' );
-	        elseif( get_row_layout() == 'news_listing' ):
-	        	get_template_part( 'templates/parts/load_news' );
-	        elseif( get_row_layout() == 'button_call_to_action' ):
-	        	get_template_part( 'templates/parts/button_cta' );
-	        elseif( get_row_layout() == 'general_content' ):
-	        	get_template_part( 'templates/parts/general_content' );
-	        elseif( get_row_layout() == 'callout' ):
-				get_template_part( 'templates/parts/callout');
-	        elseif( get_row_layout() == 'two_column_content' ):
-	        	get_template_part( 'templates/parts/two_column_content' );
-	        elseif( get_row_layout() == 'founders' ):
-	        	get_template_part( 'templates/parts/founders' );
-	        endif;
-	    endwhile;
+			endif;
+		endwhile;
 	else :
-	    // no layouts found
+		// no layouts found
 	endif;
 
 	/*if (have_posts()) :
@@ -50,6 +32,4 @@
 		endwhile;
 	endif;*/
 	?>
-
-
 <?php get_footer(); ?>
