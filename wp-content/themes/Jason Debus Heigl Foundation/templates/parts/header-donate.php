12c48639c721960f892d99f7f454d7fd3fcc36fa
jcaiaffa <jonathan.caiaffa@akendos.com>
2017-01-24 02:28:22 -0500

    Adding container to header donate part

--- wp-content/themes/Jason Debus Heigl Foundation/templates/parts/header-donate.php
+++ wp-content/themes/Jason Debus Heigl Foundation/templates/parts/header-donate.php
@@ -11,12 +11,16 @@ if( get_sub_field('global') == 1):
 		if( get_row_layout() == 'header' ):
 			?>
 				<div class="header" style="background:url(<?php the_sub_field('background_image') ?>)">
-					<div class="row">
-						<div class="col-md-12">
-							<div class="text">
-								<?php echo do_shortcode(get_sub_field('heading')) ?>
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+								<div class="donation-basic">
+									<div class="text">
+										<?php echo do_shortcode(get_sub_field('heading')) ?>
+									</div>
+								</div>
 							</div>
-						</div>
+						</div>						
 					</div>
 				</div>
 			<?php
@@ -25,13 +29,15 @@ if( get_sub_field('global') == 1):
 else :
 ?>
 <div class="header" style="background:url(<?php echo the_sub_field('background_image') ?>)">
-	<div class="row">
-		<div class="col-md-12">
-			<div class="donation-basic">
-				<div class="text">
-					<?php echo do_shortcode(get_sub_field('heading')) ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="donation-basic">
+					<div class="text">
+						<?php echo do_shortcode(get_sub_field('heading')) ?>
+					</div>
 				</div>
-			</div>
+			</div>			
 		</div>
 	</div>
 </div>
