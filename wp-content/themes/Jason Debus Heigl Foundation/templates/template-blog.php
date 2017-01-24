ceb93d0cb7e352aaea5cc003393b622cdc07153e
jcaiaffa <jonathan.caiaffa@akendos.com>
2017-01-23 22:40:45 -0500

    Blog Template fix

--- wp-content/themes/Jason Debus Heigl Foundation/templates/template-blog.php
+++ wp-content/themes/Jason Debus Heigl Foundation/templates/template-blog.php
@@ -122,8 +122,8 @@
 						$paged = ( get_query_var( 'paged' ) ) ? absint( get_query_var( 'paged' ) ) : 1;
 						$args = Array(
 						  'post_type' => 'post',
-						  'posts_per_page' => '0',
-						  'offset' => '2',
+						  'posts_per_page' => '1',
+						  'offset' => '1',
   						  'paged' => $paged
 						);
 						// The Query
@@ -162,16 +162,20 @@
 						}
 					?>
 				</div>
-				<?php
-					$big = 999999999; // need an unlikely integer
+				<div class="pagination">
+					<?php
+						$big = 999999999; // need an unlikely integer
 
-					echo paginate_links( array(
-						'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-						'format' => '?paged=%#%',
-						'current' => max( 1, get_query_var('paged') ),
-						'total' => $the_query->max_num_pages
-					) ); 
-				?>
+						echo paginate_links( array(
+							'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+							'format' => '?paged=%#%',
+							'current' => max( 1, get_query_var('paged') ),
+							'total' => $the_query->max_num_pages,
+							'prev_text'  => __('<'),
+							'next_text' => __('>')
+						) ); 
+					?>
+				</div>
 			</div>
 		</div>
 	</div>	
