5dbc1e0050c48cca3feb87faf5532c448f05043c
jcaiaffa <jonathan.caiaffa@akendos.com>
2017-01-23 19:33:20 -0500

    Our Work panels fix

--- wp-content/themes/Jason Debus Heigl Foundation/templates/parts/bio-column-left.php
+++ wp-content/themes/Jason Debus Heigl Foundation/templates/parts/bio-column-left.php
@@ -4,13 +4,13 @@ if( get_sub_field('global') == 1):
 		if( get_row_layout() == 'bio_column_right' ):
 			?>
 				<div class="bio-container" style="background-color:<?php the_sub_field('bio_background_color')?>">
-					<div class="left-half-r-container">
-						<div class="left-half-r" style="background-image: url(<?php echo the_sub_field('bio_image')?>"></div>	
-					</div>
-					<div class="right-half-r">
+					<div class="right-half-l">
 						<div class="content">
 							<?php the_sub_field('bio_text')?>
 						</div>	
+					</div>	
+					<div class="left-half-l-container">
+						<div class="left-half-l" style="background-image: url(<?php echo the_sub_field('bio_image')?>"></div>	
 					</div>
 				</div>
 			<?php
