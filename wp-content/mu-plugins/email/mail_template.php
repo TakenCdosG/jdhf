660edf72e1d9659a96097aec79cb473b697ba133
Esteban Rocha <estebanrocha.j@gmail.com>
2017-02-09 01:12:41 -0500

    BH 78 email 4
    		Yet another fix for the copy on the mail

--- wp-content/mu-plugins/email/mail_template.php
+++ wp-content/mu-plugins/email/mail_template.php
@@ -3,10 +3,12 @@
 <?php if ( $user->first_name != '' ) : ?>
 	<h1><?php echo $user->first_name; ?>, Welcome to the Jason Debus Heigl Foundation</h1>
 <?php else : ?>
-	<h1><?php echo $user->user_login; ?>, Welcome to the Jason Debus Heigl Foundation</h1>
+	<h1>Welcome to the Jason Debus Heigl Foundation</h1>
 <?php endif; ?>
+
+<p>Your username is: <?php echo $user->user_login; ?></p>
 <p> <a href="<?php echo $siteUrl; ?>/login/">Click Here to Login</a> </p>
 <p>
-	Thank you,<br>
+	We're glad you're here. Thank you,<br>
 	<a href="<?php echo $siteUrl; ?>">Jason Debus Heigl Foundation</a>
 </p>
